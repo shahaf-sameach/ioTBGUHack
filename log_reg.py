c2cda05d08b6977f464698834c3e887ef894e5fe
shahaf <shahaf@minute.ly>
2018-05-03 23:28:26 +0300

    modified log reg

--- log_reg.py
+++ log_reg.py
@@ -1,61 +1,37 @@
+import pandas as pd
+import numpy as np
+import pdb
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
 
-import tensorflow as tf
+selected_category = 'socket'
 
-# Import MNIST data
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+df_trn = pd.read_csv('hackathon_IoT_training_set_based_on_01mar2017.csv', low_memory=False, na_values='?')
+df_trn.loc[df_trn['device_category'] != selected_category ,'device_category'] = 'unk'
 
-# Parameters
-learning_rate = 0.01
-training_epochs = 25
-batch_size = 100
-display_step = 1
+watch = df_trn.loc[df_trn['device_category'] == selected_category]
+unk = df_trn.loc[df_trn['device_category'] != selected_category]
+unk = unk.iloc[:watch.shape[0], :]
 
-# tf Graph Input
-x = tf.placeholder(tf.float32, [None, 784]) # mnist data image of shape 28*28=784
-y = tf.placeholder(tf.float32, [None, 10]) # 0-9 digits recognition => 10 classes
+tmp_df_trn = pd.concat([unk, watch])
 
-# Set model weights
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
+x_trn = tmp_df_trn.iloc[:, 0:(df_trn.shape[1]-1)].copy()
+x_trn.fillna(value=0, inplace=True)
 
-# Construct model
-pred = tf.nn.softmax(tf.matmul(x, W) + b) # Softmax
+y_trn = tmp_df_trn.iloc[:,(df_trn.shape[1]-1)].copy() # last column
 
-# Minimize error using cross entropy
-cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(pred), reduction_indices=1))
-# Gradient Descent
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+categories = tmp_df_trn.device_category.unique()
+y_one_hot = [np.where(categories==category)[0][0] for category in y_trn]
 
-# Initialize the variables (i.e. assign their default value)
-init = tf.global_variables_initializer()
+X_train, X_test, y_train, y_test = train_test_split(x_trn, y_one_hot)
 
-# Start training
-with tf.Session() as sess:
+#pdb.set_trace()
+print("running")
+# train logistic regression
+logmodel = LogisticRegression()
+logmodel.fit(X_train, y_train)
+print("test" ,logmodel.score(X_train, y_train))
+print("train", logmodel.score(X_test, y_test))
 
-    # Run the initializer
-    sess.run(init)
-
-    # Training cycle
-    for epoch in range(training_epochs):
-        avg_cost = 0.
-        total_batch = int(mnist.train.num_examples/batch_size)
-        # Loop over all batches
-        for i in range(total_batch):
-            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-            # Run optimization op (backprop) and cost op (to get loss value)
-            _, c = sess.run([optimizer, cost], feed_dict={x: batch_xs,
-                                                          y: batch_ys})
-            # Compute average loss
-            avg_cost += c / total_batch
-        # Display logs per epoch step
-        if (epoch+1) % display_step == 0:
-            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
-
-    print("Optimization Finished!")
-
-    # Test model
-    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-    # Calculate accuracy
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))

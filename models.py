88597606af2f93f1f934178e32fe7fae157f3a06
shahaf <shahaf@minute.ly>
2018-05-03 16:27:32 +0300

    did everything

--- models.py
+++ models.py
@@ -8,14 +8,14 @@ from data_handler import get_data
 
 x_trn, y_trn = get_data()
 
-# train a random forest
-RFmodel = RandomForestClassifier(n_estimators=50)
-RFmodel.fit(x_trn, y_trn)
-
+# # train a random forest
+# RFmodel = RandomForestClassifier(n_estimators=50)
+# RFmodel.fit(x_trn, y_trn)
+print("running")
 # train logistic regression
 logmodel=LogisticRegression()
 logmodel.fit(x_trn, y_trn)
 
-# train knn
-knn = KNeighborsClassifier(n_neighbors=9)
-knn.fit(x_trn, y_trn)
+# # train knn
+# knn = KNeighborsClassifier(n_neighbors=9)
+# knn.fit(x_trn, y_trn)
